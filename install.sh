4684797d3927f4f8e3f509c40935ef84da775f88
Filippo Liverani <fili_live@yahoo.it>
2013-11-25 11:56:00 +0100

    updated packages list

--- install.sh
+++ install.sh
@@ -42,10 +42,10 @@ BASE_PACKAGES="pacmatic linux-lts base-devel openssh openssl unrar unzip zsh \
               alsa-lib alsa-oss alsa-utils lib32-alsa-lib pulseaudio  pulseaudio-alsa lib32-libpulse lib32-alsa-plugins \
               gstreamer0.10-plugins gstreamer0.10-base-plugins gstreamer0.10-good-plugins gstreamer0.10-bad-plugins \
               xorg-server xorg-apps xorg-xinit xorg-server-utils xf86-video-nouveau xf86-video-intel xf86-video-ati xf86-input-synaptics xclip \
-              slim slim-themes archlinux-themes-slim xfce4 xfce4-goodies xfce4-screenshooter xfce4-mixer thunar-volman gvfs gksu file-roller \
+              slim slim-themes archlinux-themes-slim xfce4 xfce4-goodies xfce4-screenshooter xfce4-mixer thunar-volman gvfs gksu file-roller evince\
               zukitwo-themes faenza-icon-theme faenza-xfce-addon ttf-dejavu artwiz-fonts xcursor-vanilla-dmz lib32-gtk2 \
               wicd-gtk pavucontrol keepassx kupfer simple-scan inkscape gimp gcolor3 gvim python-powerline-git leafpad parole skype hotot-gtk3 \
-              chromium google-talkplugin chromium-pepper-flash-stable chromium-libpdf-stable"
+              chromium google-talkplugin chromium-pepper-flash-stable"
 DEV_PACKAGES="tmux ruby ruby-tmuxinator wemux-git vagrant dstat iotop the_silver_searcher subversion eclipse \
               virtualbox virtualbox-host-modules virtualbox-guest-iso virtualbox-ext-oracle"
 
