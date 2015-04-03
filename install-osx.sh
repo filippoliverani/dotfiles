9d96dfcf8bbbb02580d2a4f5e486c12fd94fa77e
Filippo Liverani <fili_live@yahoo.it>
2015-04-03 09:49:44 +0200

    updated osx install script

---
+++ install-osx.sh
@@ -0,0 +1,86 @@
+#!/bin/bash 
+set -e
+
+tee ~/.profile  << '
+export PATH=/usr/local/bin:/usr/local/sbin:$PATH
+'
+
+# config
+
+defaults write com.apple.finder AppleShowAllFiles -bool true
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+defaults write com.apple.finder ShowStatusBar -bool true
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
+defaults write com.apple.Finder QuitMenuItem -bool true
+
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+defaults write NSGlobalDomain AppleFontSmoothing -int 2
+defaults write NSGlobalDomain KeyRepeat -int 0
+
+defaults write com.apple.dock expose-animation-duration -float 0.1
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0
+
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+defaults write com.apple.universalaccess reduceTransparency -bool true
+defaults write com.apple.BezelServices kDimTime -int 300
+
+defaults write -g com.apple.trackpad.scaling 2
+defaults write -g com.apple.mouse.scaling 2.5
+
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+defaults write com.apple.Safari UniversalSearchEnabled -bool false
+defaults write com.apple.Safari SuppressSearchSuggestions -bool true
+defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
+defaults write com.google.Chrome.canary AppleEnableSwipeNavigateWithScrolls -bool false
+defaults write com.google.Chrome DisablePrintPreview -bool true
+defaults write com.google.Chrome.canary DisablePrintPreview -bool true
+
+sudo pmset -a sms 0
+hash tmutil &> /dev/null && sudo tmutil disablelocal
+
+#brew
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
+brew install psutils
+brew install git
+brew install ruby
+brew install python
+brew install tmux
+brew install the_silver_searcher
+brew install vim --with-python --with-ruby --with-perl --env-std --override-system-vim
+brew install caskroom/cask/brew-cask
+
+#cask
+
+brew cask install amethyst
+brew cask install caffeine
+brew cask install google-chrome
+brew cask install dropbox
+brew cask install flux
+brew cask install java
+brew cask install skype
+brew cask install the-unarchiver
+brew cask install vagrant
+brew cask install virtualbox
+
+#powerline
+
+pip install git+git://github.com/Lokaltog/powerline
+pip install psutil
+
+mkdir -p ~/.config/powerline
+cp -R /usr/local/lib/python2.7/site-packages/powerline/config_files/** ~/.config/powerline
+sudo mkdir -p /usr/share/tmux
+sudo ln -s /usr/local/lib/python2.7/site-packages/powerline/bindings/tmux/powerline.conf /usr/share/tmux/powerline.conf
